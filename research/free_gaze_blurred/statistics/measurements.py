44f4ad27514324fa6461e4612adae8f86ac79e69
LiorLevi15 <levilior15@gmail.com>
2022-08-12 20:19:21 +0300

    final push

--- research/free_gaze_blurred/statistics/measurements.py
+++ research/free_gaze_blurred/statistics/measurements.py
@@ -9,50 +9,63 @@ from scipy.spatial.distance import cdist
 
 
 def print_results():
-    start_time = time.time()
-    avg_results_by_blur, results = calc_results()
+    avg_results_by_blur, results, blinks_per_blur, saccades_per_blur = calc_results(span=2)
+    labels = ["0.0-2.0", "2.0-4.0", "4.0-6.0", "6.0-8.0", "8.0-10.0", "10.0-12.0", "12.0-14.0", "14.0-16.0", "16.0-18.0", "18.0-20.0"]
     end_time = time.time()
-    time_convert(end_time - start_time)
-    print(f"results: {results}")
-    print(f"avg_results_by_blur: {avg_results_by_blur}")
+    # print(f"results: {results}")
+
+    # blinks per image blur level
+    plt.figure()
+    plt.scatter(blinks_per_blur.keys(), blinks_per_blur.values())
+    plt.savefig('C:\\Users\\levil\\OneDrive\\שולחן העבודה\\experiment_processing\\experiment_processing\\research\\free_gaze_blurred\\statistics\\blinks_per_blur.png')
+
+    # saccades per image blur level
+    plt.figure()
+    plt.scatter(saccades_per_blur.keys(), saccades_per_blur.values())
+    plt.savefig(
+        'C:\\Users\\levil\\OneDrive\\שולחן העבודה\\experiment_processing\\experiment_processing\\research\\free_gaze_blurred\\statistics\\saccades_per_blur.png')
 
     # linear distance heat map
-    matrix = get_measurement_results_matrix("linear distance", avg_results_by_blur)
-    ax = sns.heatmap(matrix, linewidth=0.5)
-    plt.show()
+    matrix1 = get_measurement_results_matrix("linear distance", avg_results_by_blur)
+    plt.figure(figsize=(12, 12))
+    ax = sns.heatmap(matrix1, linewidth=1, square=False, annot=True, fmt=".1f", xticklabels=labels, yticklabels=labels)
+    plt.savefig(
+        'C:\\Users\\levil\\OneDrive\\שולחן העבודה\\experiment_processing\\experiment_processing\\research\\free_gaze_blurred\\statistics\\linear_distance.png')
 
     # cross recurrence
-    matrix = get_measurement_results_matrix("cross recurrence", avg_results_by_blur)
-    ax = sns.heatmap(matrix, linewidth=0.5)
-    plt.show()
+    matrix2 = get_measurement_results_matrix("cross recurrence", avg_results_by_blur)
+    plt.figure(figsize=(12, 12))
+    ax = sns.heatmap(matrix2, linewidth=1, square=True, annot=True, fmt=".1f", xticklabels=labels, yticklabels=labels)
+    plt.savefig(
+        'C:\\Users\\levil\\OneDrive\\שולחן העבודה\\experiment_processing\\experiment_processing\\research\\free_gaze_blurred\\statistics\\cross_recurrence.png')
 
     # determinism
-    matrix = get_measurement_results_matrix("determinism", avg_results_by_blur)
-    ax = sns.heatmap(matrix, linewidth=0.5)
-    plt.show()
+    matrix3 = get_measurement_results_matrix("determinism", avg_results_by_blur)
+    plt.figure(figsize=(12, 12))
+    ax = sns.heatmap(matrix3, linewidth=1, square=True, annot=True, fmt=".1f", xticklabels=labels, yticklabels=labels)
+    plt.savefig(
+        'C:\\Users\\levil\\OneDrive\\שולחן העבודה\\experiment_processing\\experiment_processing\\research\\free_gaze_blurred\\statistics\\determinism.png')
 
     # laminarity
-    matrix = get_measurement_results_matrix("laminarity", avg_results_by_blur)
-    ax = sns.heatmap(matrix, linewidth=0.5)
-    plt.show()
+    matrix4 = get_measurement_results_matrix("laminarity", avg_results_by_blur)
+    plt.figure(figsize=(12, 12))
+    ax = sns.heatmap(matrix4, linewidth=1, square=True, annot=True, fmt=".1f", xticklabels=labels, yticklabels=labels)
+    plt.savefig(
+        'C:\\Users\\levil\\OneDrive\\שולחן העבודה\\experiment_processing\\experiment_processing\\research\\free_gaze_blurred\\statistics\\laminarity.png')
 
     # center of recurrence mas
-    matrix = get_measurement_results_matrix("CORM", avg_results_by_blur)
-    ax = sns.heatmap(matrix, linewidth=0.5)
-    plt.show()
+    matrix5 = get_measurement_results_matrix("CORM", avg_results_by_blur)
+    plt.figure(figsize=(12, 12))
+    ax = sns.heatmap(matrix5, linewidth=1, annot=True, fmt=".1f", xticklabels=labels, yticklabels=labels)
+    plt.savefig(
+        'C:\\Users\\levil\\OneDrive\\שולחן העבודה\\experiment_processing\\experiment_processing\\research\\free_gaze_blurred\\statistics\\corm.png')
 
     # fixation overlap
-    matrix = get_measurement_results_matrix("fixation overlap", avg_results_by_blur)
-    ax = sns.heatmap(matrix, linewidth=0.5)
-    plt.show()
-
-
-def time_convert(sec):
-    mins = sec // 60
-    sec = sec % 60
-    hours = mins // 60
-    mins = mins % 60
-    print("Time Lapsed = {0}:{1}:{2}".format(int(hours), int(mins), sec))
+    matrix6 = get_measurement_results_matrix("fixation overlap", avg_results_by_blur)
+    plt.figure(figsize=(12, 12))
+    ax = sns.heatmap(matrix6, linewidth=1, square=True, annot=True, fmt=".1f", xticklabels=labels, yticklabels=labels)
+    plt.savefig(
+        'C:\\Users\\levil\\OneDrive\\שולחן העבודה\\experiment_processing\\experiment_processing\\research\\free_gaze_blurred\\statistics\\fixation_overlap.png')
 
 
 def get_measurement_results_matrix(measurement, avg_results_by_blur):
@@ -61,18 +74,23 @@ def get_measurement_results_matrix(measurement, avg_results_by_blur):
             for j in range(len(avg_results_by_blur))]
 
 
-def calc_results():
+def calc_results(span=1):
     fixations_df = pd.read_csv(
         'C:\\Users\\levil\\OneDrive\\שולחן העבודה\\experiment_processing\\experiment_processing\\research\\free_gaze_blurred\statistics\\fixations.csv')
     images_df = pd.read_csv(
         'C:\\Users\\levil\\OneDrive\\שולחן העבודה\\experiment_processing\\experiment_processing\\research\\free_gaze_blurred\\statistics\\images.csv')
-    samples_df = pd.read_csv(
-        'C:\\Users\\levil\\OneDrive\\שולחן העבודה\\experiment_processing\\experiment_processing\\research\\free_gaze_blurred\\statistics\\samples.csv')
+    blinks_df = pd.read_csv(
+        'C:\\Users\\levil\\OneDrive\\שולחן העבודה\\experiment_processing\\experiment_processing\\research\\free_gaze_blurred\\statistics\\blinks.csv')
+    saccades_df = pd.read_csv(
+        'C:\\Users\\levil\\OneDrive\\שולחן העבודה\\experiment_processing\\experiment_processing\\research\\free_gaze_blurred\\statistics\\saccades.csv')
+    blinks_per_blur = blinks_per_image_blur(blinks_df, images_df)
+    saccades_per_blur = saccades_per_image_blur(saccades_df, images_df)
     # init scanpaths list
     fixations_sps_df = group_by_image(fixations_df)
     fixations_sps = [init_fixation_list(sp) for sp in fixations_sps_df]
-
-    samples_sps = group_by_image(samples_df)
+    resampled_fixations_df = resample_fixations(images_df, fixations_df)
+    resampled_fixations_sps_df = group_by_image(resampled_fixations_df)
+    resampled_fixations_sps = [init_fixation_list_with_time(sp) for sp in resampled_fixations_sps_df]
     # init matrix of measurements results for each 2 scanpaths
     results = np.array([[{} for _ in range(len(fixations_sps))] for _ in range((len(fixations_sps)))])
     # init a matrix of measurements results average values for each 2 scanpaths of a specific blur range
@@ -81,29 +99,32 @@ def calc_results():
                                           "determinism": 0,
                                           "laminarity": 0,
                                           "CORM": 0,
-                                          "fixation overlap": 0}) for _ in range(20)] for _ in range(20)])
+                                          "fixation overlap": 0}) for _ in range(20 // span)] for _ in
+                                    range(20 // span)])
     # calculate results and average results
     for i in range(len(fixations_sps)):
         for j in range(len(fixations_sps)):
             if i != j and len(fixations_sps[i]) > 0 and len(fixations_sps[j]) > 0:
                 # TODO: figure out how to calculate distance with visual angle (1.9 and 3.5)
-                crm = cross_recurrence_analysis(fixations_sps[i], fixations_sps[j], 50)
-                # overlap_percentage, _, _ = fixation_overlap(samples_sps[i], samples_sps[j], 3.5)
+                crm = cross_recurrence_analysis(fixations_sps[i], fixations_sps[j], 100)
+                overlap_percentage = fixation_overlap(resampled_fixations_sps[i], resampled_fixations_sps[j], 100)
                 measures = {"linear distance": calc_linear_distance(fixations_sps[i], fixations_sps[j]),
                             "cross recurrence": calc_cross_recurrence(crm),
-                            "determinism": calc_determinism(crm, 2),
+                            "determinism": calc_determinism(crm, 2, sec_diagonal=False),
                             "laminarity": calc_laminarity(crm, 2),
                             "CORM": calc_corm(crm),
-                            "fixation overlap": 0}  # overlap_percentage}
+                            "fixation overlap": overlap_percentage}
                 results[i][j] = measures
-                blur_idx1 = int(images_df.loc[fixations_sps_df[i].loc[0, 'image_index'], 'blur'])
-                blur_idx2 = int(images_df.loc[fixations_sps_df[j].loc[0, 'image_index'], 'blur'])
+                blur_i = images_df.loc[fixations_sps_df[i].loc[0, 'image_index'], 'blur']
+                blur_j = images_df.loc[fixations_sps_df[j].loc[0, 'image_index'], 'blur']
+                blur_idx1 = int(blur_i // span)
+                blur_idx2 = int(blur_j // span)
                 count, values = avg_results_by_blur[blur_idx1][blur_idx2]
                 for key in measures:
                     value = measures[key]
                     values[key] = ((values[key] * count) + value) / (count + 1)
                 avg_results_by_blur[blur_idx1][blur_idx2] = (count + 1, values)
-    return avg_results_by_blur, results
+    return avg_results_by_blur, results, blinks_per_blur, saccades_per_blur
 
 
 def group_by_image(df):
@@ -114,6 +135,29 @@ def group_by_image(df):
         sp.reset_index(drop=True, inplace=True)
         sps.append(sp)
     return sps
+
+
+def saccades_per_image_blur(saccades_df, images_df):
+    # add image_blur column to saccades_df
+    for index, row in saccades_df.iterrows():
+        saccades_df.loc[index, 'image_blur'] = images_df.loc[row['image_index'], 'blur']
+    saccades_df = saccades_df.astype({"image_index": int}, errors='raise')
+    # calc saccades per blur level
+    saccades_per_blur = {}
+    for index, row in saccades_df.iterrows():
+        if row['image_blur'] in saccades_per_blur:
+            saccades_per_blur[row['image_blur']] += 1
+        else:
+            saccades_per_blur[row['image_blur']] = 0
+    # normalize 0.0 blur level
+    zero_blur_count = 0
+    for index, row in images_df.iterrows():
+        if row['blur'] == 0.0:
+            zero_blur_count += 1
+    saccades_per_blur[0.0] = saccades_per_blur[0.0] / zero_blur_count
+    return saccades_per_blur
+    plt.scatter(saccades_per_blur.keys(), saccades_per_blur.values())
+    plt.show()
 
 
 def blinks_per_image_blur(blinks_df, images_df):
@@ -134,43 +178,26 @@ def blinks_per_image_blur(blinks_df, images_df):
         if row['blur'] == 0.0:
             zero_blur_count += 1
     blinks_per_blur[0.0] = blinks_per_blur[0.0] / zero_blur_count
-    # scatter plot results
-    plt.scatter(blinks_per_blur.keys(), blinks_per_blur.values())
-    plt.show()
+    return blinks_per_blur
 
 
-def fixation_overlap(sample_df1, sample_df2, radius):
+def fixation_overlap(fixations1, fixations2, radius):
     # make equal length
-    diff = sample_df1.shape[0] - sample_df2.shape[0]
-    if diff > 0:
-        sample_df1 = sample_df1.loc[0: sample_df2.shape[0] - 1]
-    elif diff < 0:
-        sample_df2 = sample_df2.loc[0: sample_df1.shape[0] - 1]
-    distances = {}
-    dist_sum = 0
-    count = 0
-    overlaps = 0
+    fixations1_copy = fixations1.copy()
+    fixations2_copy = fixations2.copy()
+    # truncate the longer list
+    del fixations1_copy[len(fixations2_copy):]
+    del fixations2_copy[len(fixations1_copy):]
+    count_overlaps = 0
     # calculates list of distances for each pair of equal index entries
-    for index1, row in sample_df1.iterrows():
-        if sample_df1.loc[index1, 'is_fixation'] and sample_df2.loc[index1, 'is_fixation']:
-            point1 = np.array((sample_df1.loc[index1, 'image_x'], sample_df1.loc[index1, 'image_y']))
-            point2 = np.array((sample_df2.loc[index1, 'image_x'], sample_df2.loc[index1, 'image_y']))
-            distances[index1] = calc_dist(point1, point2)
-            if distances[index1] <= radius:
-                overlaps += 1
-            dist_sum += distances[index1]
-            count += 1
-        else:
-            distances[index1] = None
-    count = count if count != 0 else 1
-    similarity = dist_sum / count
-    percentage = overlaps / count
-    return percentage, similarity, distances
+    for i in range(len(fixations1_copy)):
+        if overlaps(fixations1_copy[i][0], fixations1_copy[i][1], fixations2_copy[i][0], fixations2_copy[i][1], radius):
+            count_overlaps += 1
+    length = len(fixations1_copy) if len(fixations1_copy) > 0 else 1
+    return 100 * (count_overlaps / length)
 
 
 def calc_linear_distance(fixations1, fixations2, normalize=True):
-#    fixations1 = init_fixation_list(fixations_df1)
-#    fixations2 = init_fixation_list(fixations_df2)
     distances = cdist(fixations1, fixations2)
     d = distances.min(axis=0).sum() + distances.min(axis=1).sum()
     if normalize:
@@ -196,16 +223,11 @@ def calc_determinism(crm, length, by_length=True, sec_diagonal=True):
     return 100 * (count_length / c) if by_length else 100 * (count / c)
 
 
-def calc_laminarity(crm, length, by_length=True, sec_diagonal=True):
-    count, count_length = calc_horizontal_lines(crm, length)
+def calc_laminarity(crm, length, by_length=True):
+    count, count_length = calc_vertical_lines(crm, length)
     count2, count_length2 = calc_diagonal_lines(crm, length)
     count += count2
     count_length += count_length2
-    if sec_diagonal:
-        flipped_crm = np.fliplr(crm)
-        count2, count_length2 = calc_diagonal_lines(flipped_crm, length)
-        count += count2
-        count_length += count_length2
     c = crm.sum()
     c = c if c != 0 else 1
     return 100 * (count_length / c) if by_length else 100 * (count / c)
@@ -252,6 +274,18 @@ def calc_horizontal_lines(matrix, length):
     return count, count_length
 
 
+def calc_vertical_lines(matrix, length):
+    n = len(matrix)
+    count = 0
+    count_length = 0
+    for i in range(n):
+        horizontal = matrix[:, i]
+        temp_count, temp_count_length = calc_ones(horizontal, length)
+        count += temp_count
+        count_length += temp_count_length
+    return count, count_length
+
+
 def calc_ones(vector, length):
     index = 0
     count = 0
@@ -269,8 +303,6 @@ def calc_ones(vector, length):
 
 
 def cross_recurrence_analysis(fixations1, fixations2, radius):
-#    fixations1 = init_fixation_list(sample_df1)
-#    fixations2 = init_fixation_list(sample_df2)
     fixations1_copy = fixations1.copy()
     fixations2_copy = fixations2.copy()
     # truncate the longer list
@@ -293,6 +325,15 @@ def init_fixation_list(fixation_df):
     return fixations
 
 
+def init_fixation_list_with_time(fixation_df):
+    fixations = []
+    for index, row in fixation_df.iterrows():
+        point = np.array((fixation_df.loc[index, 'image_x'], fixation_df.loc[index, 'image_y']))
+        interval = pd.Interval(fixation_df.loc[index, 'start time'], fixation_df.loc[index, 'end time'])
+        fixations.append([point, interval])
+    return fixations
+
+
 def resample_fixations(images_df, fixations_df):
     ret_df = fixations_df.copy()
     for index, _ in ret_df.iterrows():
@@ -302,17 +343,13 @@ def resample_fixations(images_df, fixations_df):
     return ret_df
 
 
+def overlaps(point1, interval1, point2, interval2, radius):
+    return interval1.overlaps(interval2) and calc_dist(point1, point2) <= radius
+
+
 def calc_dist(point1, point2):
     return np.linalg.norm(point1 - point2)
 
 
-def testing():
-    matrix = np.zeros(181, 181)
-    print(int(1.9))
-
-
 if __name__ == '__main__':
-    # testing()
     print_results()
-    # blinks_per_image()
-    #
